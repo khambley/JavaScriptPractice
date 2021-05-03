741a516b7c6925efb76e300103f2f62ab30ee551
Katherine Hambley <superdesigngirl@mac.com>
2021-05-03 08:45:26 -0500

    Added starter code for Rock Paper Scissors x99 game

---
+++ 01-Project-RockPaperScissorX99/js/game-logic.js
@@ -0,0 +1,25 @@
+// All code should be written in this file.
+let playerOneMoveOneType;
+let playerOneMoveOneValue;
+let playerOneMoveTwoType;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeType;
+let playerOneMoveThreeValue;
+
+let playerTwoMoveOneType;
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoType;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeType;
+let playerTwoMoveThreeValue;
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue){
+  if(!moveOneType || !moveOneValue || !moveTwoType || !moveTwoValue || !moveThreeType || !moveThreeValue){
+    return;
+  }
+  
+}
+
+function getGameWinner(){
+
+}
